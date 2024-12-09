e6c7d735b54ba9a6eaa7972dc5f5940c8e399206
Santiago Mesa Parra <86623637+SantiagoMesa0911@users.noreply.github.com>
2024-12-08 20:22:20 -0500

    Update Usuarios.js

--- Routes/Usuarios.js
+++ Routes/Usuarios.js
@@ -18,7 +18,7 @@ function Usuarios(app) {
         return res.status(200).json(usuarios)
     })
 
-    router.post('/crear', async (req, res) => {
+    router.post('/crear',  authenticate, authorize(['administrador']), async (req, res) => {
         const nuevo_usuario = await UsuarioServicio.CrearUsuarios(req.body)
         if (nuevo_usuario.error) {
             return res.status(400).json({ error: nuevo_usuario.error });
